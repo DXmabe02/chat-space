a31b269c2ca72681a412152eb4efe4b8d71aa10c
DXnabe02 <watanabe02masahide02@gmail.com>
2020-05-29 12:20:11 +0900

    Change user.js and users_controller.rb

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,6 +2,17 @@ $(function(){
 
   $('#user-search-field').on('keyup', function(){
     var input = $("#user-search-field").val();//フォームの値を取得して変数に代入する
-    console.log(input);
+    $.ajax({
+      type: 'GET',    //HTTPメソッド
+      url: '/users',       //users_controllerの、indexアクションにリクエストの送信先を設定する
+      data: { keyword: input },   //テキストフィールドに入力された文字を設定する
+      dataType: 'json',
+    })
+    .done(function(users) {
+      console.log("成功です");
+    })
+    .fail(function() {
+      console.log("失敗です");
+    });
   })
 });
